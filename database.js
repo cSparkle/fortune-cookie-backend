e057eec4612c8f85f36011994f78c38fe620b867
Cortney <thomas.cortney@gmail.com>
2022-12-21 18:16:38 -0500

    first commit

---
+++ database.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'fortune-cookie';
+
+MongoClient.connect(connectionURL, {
+    useNewUrlParser: true
+}, (error, client) => {
+    if (error) {
+        return console.error("Unable to connect to DB: ", error);
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection('words').deleteOne({
+    //     word: "shook"
+    // })
+
+    // db.collection('fortunes').insertOne({
+    //     phrase: "You're fucked."
+    // })
+
+    // db.collection('words').insertOne({
+    //     category: "millennial",
+    //     pronunciation: "♡ Live Laugh Love ♡",
+    //     word: "lit"
+    // })
+
+    // db.collection('words').updateOne({
+    //     word: "adulting"
+    // }, {
+    //     $set: {
+    //         pronunciation: "I'll put it on my credit card."
+    //     }
+    // })
+})
